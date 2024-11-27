756a52b87cd4dde4820dfc15d25097e9c7fde12d
clang-engineer <clang.engineer@gmail.com>
2024-11-27 14:07:47 +0900

    feat: temp

--- src/main/kotlin/io/clang_engineer/quartz_explorer/QuartzExplorerApplication.kt
+++ src/main/kotlin/io/clang_engineer/quartz_explorer/QuartzExplorerApplication.kt
@@ -15,40 +15,94 @@ fun main(args: Array<String>) {
   val scheduler = context.getBean(org.quartz.Scheduler::class.java)
 
   while (true) {
-    println("Type 'exit' to stop the application")
-    val input = readLine()
+    println("Type 'start' to schedule a job, 'scheduler' to control the scheduler, 'trigger' to control a trigger, 'job' to control a job, 'list' to list all jobs, or 'exit' to exit.")
+    val type = readLine()
 
-    if (input.isNullOrBlank()) {
+    if (type.isNullOrBlank()) {
       println("Invalid input. Please try again.")
       continue
     }
 
-    if (input == "exit") {
-      exitProcess(0)
-    }
-
-    if (input == "start") {
-      schedulerService.scheduleJob(SampleJob::class.java, "0/5 * * * * ?")
-    }
-
-    if (input == "stop") {
-      scheduler.shutdown()
-    }
-
-    if (input == "pause") {
-      scheduler.pauseAll()
-    }
-
-    if (input == "resume") {
-      scheduler.resumeAll()
-    }
-
-    if (input == "list") {
-      val jobKeys = scheduler.getJobKeys(org.quartz.impl.matchers.GroupMatcher.anyJobGroup())
-      jobKeys.forEach { jobKey ->
-        val jobDetail = scheduler.getJobDetail(jobKey)
-        println("Job: ${jobDetail.key}")
+    when (type) {
+      "exit" -> exitProcess(0)
+      "start" -> schedulerService.scheduleJob(SampleJob::class.java, "0/5 * * * * ?")
+      "scheduler" -> controlScheduler(scheduler)
+      "trigger" -> controlTrigger(scheduler)
+      "job" -> controlJob(scheduler)
+      "list" -> {
+        val jobKeys = scheduler.getJobKeys(org.quartz.impl.matchers.GroupMatcher.anyJobGroup())
+        jobKeys.forEach { jobKey ->
+          val jobDetail = scheduler.getJobDetail(jobKey)
+          println("Job: ${jobDetail.key}")
+        }
       }
+    }
+  }
+}
+
+fun controlScheduler(scheduler: org.quartz.Scheduler) {
+  println("Enter action (start, stop, standby):")
+  val action = readLine()
+
+  when (action) {
+    "start" -> scheduler.start()
+    "stop" -> scheduler.shutdown()
+    "standby" -> scheduler.standby()
+    "pause" -> scheduler.pauseAll()
+    "resume" -> scheduler.resumeAll()
+  }
+}
+
+fun controlTrigger(scheduler: org.quartz.Scheduler) {
+  println("Enter trigger key:")
+  val triggerKey = org.quartz.TriggerKey(readLine())
+  println("Enter action (pause, resume, delete):")
+  val action = readLine()
+
+  when (action) {
+    "pause" -> scheduler.pauseTrigger(triggerKey)
+    "resume" -> scheduler.resumeTrigger(triggerKey)
+    "delete" -> scheduler.unscheduleJob(triggerKey)
+    "reschedule" -> {
+      println("Enter cron expression:")
+      val cronExpression = readLine()
+      val trigger = org.quartz.TriggerBuilder.newTrigger()
+        .withIdentity(triggerKey)
+        .withSchedule(org.quartz.CronScheduleBuilder.cronSchedule(cronExpression))
+        .build()
+      scheduler.rescheduleJob(triggerKey, trigger)
+    }
+  }
+}
+
+fun controlJob(scheduler: org.quartz.Scheduler) {
+  println("Enter job key:")
+  val jobKey = org.quartz.JobKey(readLine())
+  println("Enter action (pause, resume, delete):")
+  val action = readLine()
+
+  when (action) {
+    "pause" -> scheduler.pauseJob(jobKey)
+    "resume" -> scheduler.resumeJob(jobKey)
+    "delete" -> scheduler.deleteJob(jobKey)
+    "trigger" -> {
+      val trigger = org.quartz.TriggerBuilder.newTrigger()
+        .forJob(jobKey)
+        .withIdentity(jobKey.name + "Trigger")
+        .withSchedule(org.quartz.CronScheduleBuilder.cronSchedule("0/5 * * * * ?"))
+        .build()
+      scheduler.scheduleJob(trigger)
+    }
+
+    "reschedule" -> {
+      println("Enter cron expression:")
+      val cronExpression = readLine()
+      val trigger = org.quartz.TriggerBuilder.newTrigger()
+        .forJob(jobKey)
+        .withIdentity(jobKey.name + "Trigger")
+        .withSchedule(org.quartz.CronScheduleBuilder.cronSchedule(cronExpression))
+        .build()
+      scheduler.rescheduleJob(org.quartz.TriggerKey(jobKey.name + "Trigger"), trigger)
     }
   }
 }

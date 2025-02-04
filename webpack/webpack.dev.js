609c24e4a95fb3f641983c25870fe15fe147b4d5
clang-engineer <clang.engineer@gmail.com>
2025-02-04 20:17:21 +0900

    feat: default webpack layout

---
+++ webpack/webpack.dev.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, '../src/main/webapp/app/index.js'),
+  mode: 'development',
+  devServer: {
+    port: 9000,
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../src/main/webapp/index.html'),
+      title: 'Webpack Dev Server Example',
+    }),
+  ],
+};

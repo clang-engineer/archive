7bb5ebc930980da1e886dee96c5e34d414986585
clang-engineer <clang.engineer@gmail.com>
2025-02-04 22:56:12 +0900

    feat: minimum react config for use react on webpack typescript

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -2,17 +2,17 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, '../src/main/webapp/app/index.ts'),
+  entry: path.resolve(__dirname, '../src/main/webapp/app/index.tsx'),
   output: {
     path: path.resolve(__dirname, '../build/resources/main/static/'),
   },
   resolve: {
-    extensions: ['.ts', '.js'], // .ts 확장자를 인식하도록 설정
+    extensions: ['.tsx', '.ts', '.js'],
   },
   module: {
     rules: [
       {
-        test: /\.ts$/, // TypeScript 파일을 처리
+        test: /\.(ts|tsx)$/,
         use: 'ts-loader', // ts-loader 사용
         exclude: /node_modules/, // node_modules는 제외
       },

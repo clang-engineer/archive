bcecdac9a5b96d2ad5841ed5f596f94b31eb3424
clang-engineer <clang.engineer@gmail.com>
2025-02-04 23:49:02 +0900

    refactor: split app.tsx from index.tsx

--- src/main/webapp/app/index.tsx
+++ src/main/webapp/app/index.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
-import AppRoutes from "./routes";
-import {BrowserRouter} from 'react-router-dom';
 import {createRoot} from 'react-dom/client';
-
-
-const App = () => <>
-  <BrowserRouter>
-    <AppRoutes/>
-  </BrowserRouter>
-</>;
+import App from "./app";
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App/>);

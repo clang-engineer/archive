a7883b80d4813e344e0c5eec0378130f8148ada8
clang-engineer <clang.engineer@gmail.com>
2025-02-04 20:47:29 +0900

    feat: config for use typescript (ts-loader)

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -2,9 +2,21 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, '../src/main/webapp/app/index.js'),
+  entry: path.resolve(__dirname, '../src/main/webapp/app/index.ts'),
   output: {
     path: path.resolve(__dirname, '../build/resources/main/static/'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'], // .ts 확장자를 인식하도록 설정
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/, // TypeScript 파일을 처리
+        use: 'ts-loader', // ts-loader 사용
+        exclude: /node_modules/, // node_modules는 제외
+      },
+    ],
   },
   mode: 'development',
   devServer: {

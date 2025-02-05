32cb2ada80d56f690195a638f22cce1a517e3fa4
clang-engineer <clang.engineer@gmail.com>
2025-02-05 12:26:07 +0900

    fix: navigation.tsx - blinkin

--- src/main/webapp/app/shared/layout/navigation.tsx
+++ src/main/webapp/app/shared/layout/navigation.tsx
@@ -1,22 +1,36 @@
 import React from 'react';
 
+import {useLocation, useNavigate} from 'react-router-dom';
+
 import {Nav} from "tabler-react";
 
+const CustomNavItem = ({to, children, icon}: any) => {
+  const location = useLocation();
+  const navigation = useNavigate();
+  return (
+      <Nav.Item icon={icon} active={location.pathname === to}
+                onClick={() => {
+                  navigation(to);
+                }}>
+        {children}
+      </Nav.Item>
+  );
+};
 const Navigation = () => {
   return (
-      <Nav>
-        <Nav.Item to="/" icon="home">
+      <Nav className="p-2">
+        <CustomNavItem to="/" icon="home">
           Home
-        </Nav.Item>
-        <Nav.Item to="/test1" icon="home">
+        </CustomNavItem>
+        <CustomNavItem to="/test1" icon="home">
           Test 1
-        </Nav.Item>
-        <Nav.Item to="/test2" icon="home">
+        </CustomNavItem>
+        <CustomNavItem to="/test2" icon="home">
           Test 2
-        </Nav.Item>
-        <Nav.Item to="/test3" icon="home">
+        </CustomNavItem>
+        <CustomNavItem to="/test3" icon="home">
           Test 3
-        </Nav.Item>
+        </CustomNavItem>
       </Nav>
   );
 }

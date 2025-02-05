1f40f4133993772114964e4982972ecc8f9ab3d2
clang-engineer <clang.engineer@gmail.com>
2025-02-05 11:44:07 +0900

    chore: navigation css

--- src/main/webapp/app/shared/layout/navigation.tsx
+++ src/main/webapp/app/shared/layout/navigation.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 import "./navigation.scss";
 
+function CustomNavLink({ to, children }) {
+  return (
+      <NavLink to={to} className={({ isActive }) => (isActive ? "active" : "")}>
+        {children}
+      </NavLink>
+  );
+}
 const Navigation = () => {
   return (
       <div className="navigation-wrapper">
         <div className="item">
-          <a href="/">Home</a>
+          <CustomNavLink to="/">Home</CustomNavLink>
         </div>
         <div className="item">
-          <a href="/test1">Test 1</a>
+          <CustomNavLink to="/test1">Test 1</CustomNavLink>
         </div>
         <div className="item">
-          <a href="/test2">Test 2</a>
+          <CustomNavLink to="/test2">Test 2</CustomNavLink>
         </div>
         <div className="item">
-          <a href="/test3">Test 3</a>
+          <CustomNavLink to="/test3">Test 3</CustomNavLink>
         </div>
       </div>
   );

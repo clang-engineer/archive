1ca1dee8a759914479919411ef4ff12a3c6362e2
clang-engineer <clang.engineer@gmail.com>
2025-02-15 19:48:17 +0900

    chore: tab layout

--- src/main/webapp/app/shared/layout/navigation.tsx
+++ src/main/webapp/app/shared/layout/navigation.tsx
@@ -8,10 +8,12 @@ const NavLink = ({to, children, icon}: any) => {
   const navigate = useNavigate();
 
   const isActive = location.pathname === to;
+
+  const activeClass = isActive ? 'text-indigo-500 border-b-2 border-b-indigo-500' : 'text-gray-700 hover:bg-indigo-100 hover:text-indigo-500 hover:border-b-indigo-500 hover:border-b-2';
+
   return (
       <li className="me-2" role="presentation">
-        <button className={`p-4 border-b-2 rounded-t-lg flex focus:ring-0 hover:cursor-pointer
-                ${isActive ? 'text-indigo-500' : 'text-gray-700 hover:bg-indigo-100 hover:text-indigo-500'}`}
+        <button className={`p-4 rounded-t-lg flex focus:ring-0 hover:cursor-pointer ${activeClass}`}
                 tabIndex={-1}
                 id="profile-styled-tab"
                 type="button" role="tab" aria-controls="profile"
@@ -30,8 +32,8 @@ const NavLink = ({to, children, icon}: any) => {
 const Navigation = () => {
 
   return (
-      <div className="mb-4 border-b border-gray-200 dark:border-gray-700">
-        <ul className="flex flex-wrap -mb-px text-sm font-medium text-center"
+      <div className="mb-4 border-b border-gray-300">
+        <ul className="flex flex-wrap -mb-px m-0 text-sm font-medium text-center"
             id="default-styled-tab">
           <NavLink to="/" icon={<IconHome/>}>
             Home

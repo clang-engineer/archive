d1b31d9fb4722113650fa68053f247c329ee57e9
clang-engineer <clang.engineer@gmail.com>
2025-02-17 16:20:11 +0900

    feat: Datasource - post api layout

--- src/main/kotlin/io/clang_engineer/batch_explorer/domain/Datasource.kt
+++ src/main/kotlin/io/clang_engineer/batch_explorer/domain/Datasource.kt
@@ -1,23 +1,22 @@
 package io.clang_engineer.batch_explorer.domain
 
 import jakarta.persistence.*
-import java.time.Instant
 
 @Entity
 @Table(name = "tbl_datasource")
 data class Datasource(
-        @Id
-        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
-        @SequenceGenerator(name = "sequenceGenerator")
-        @Column(name = "id")
-        var id: Long? = null,
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+  @SequenceGenerator(name = "sequenceGenerator")
+  @Column(name = "id")
+  var id: Long? = null,
 
-        @Column(name = "title", length = 100, nullable = false)
-        var title: String? = null,
+  @Column(name = "title", length = 100, nullable = false)
+  var title: String? = null,
 
-        @Column(name = "description")
-        var description: String? = null,
+  @Column(name = "description")
+  var description: String? = null,
 
-        @Column(name = "activated")
-        var activated: Boolean? = null,
+  @Column(name = "activated")
+  var activated: Boolean? = null,
 )

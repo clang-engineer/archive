55c28295bcc27dd7c5ec5a348af9225a0a0fdfa7
clang-engineer <clang.engineer@gmail.com>
2025-03-12 13:32:32 +0900

    feat: jest layout add

---
+++ src/main/webapp/app/shared/util/entity-utils.ts
@@ -0,0 +1,11 @@
+import pick from 'lodash/pick';
+
+export const cleanEntity = entity => {
+  const keysToKeep = Object.keys(entity).filter(k => !(entity[k] instanceof Object) || (entity[k]['id'] !== '' && entity[k]['id'] !== -1));
+
+  return pick(entity, keysToKeep);
+};
+
+export const mapIdList = (idList: ReadonlyArray<any>) =>
+  idList.filter((entityId: any) => entityId !== '').map((entityId: any) => ({ id: entityId }));
+

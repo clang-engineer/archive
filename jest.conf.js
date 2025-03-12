ac49c48a2e7ee3ce7de160313ccb1bd8b4395e4c
clang-engineer <clang.engineer@gmail.com>
2025-03-12 14:44:25 +0900

    feat: jest test sample

--- jest.conf.js
+++ jest.conf.js
@@ -1,5 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  cacheDirectory: '<rootDir>/build/jest-cache',
   coverageDirectory: '<rootDir>/build/test-results/',
+  moduleNameMapper: {
+    '^app/(.*)$': '<rootDir>/src/main/webapp/app/$1',
+  },
 };

76a369e94cdd02197fa9f662bc7267c54ddf7e14
clang-engineer <clang.engineer@gmail.com>
2025-04-04 20:14:48 +0900

    feat: datasource.tsx - add button

--- src/main/webapp/app/entities/datasource/datasource.tsx
+++ src/main/webapp/app/entities/datasource/datasource.tsx
@@ -1,32 +1,43 @@
-import React, { useEffect } from 'react';
-import { useAppDispatch } from "app/config/store";
-import { getEntities } from "app/entities/datasource/datasource.reducer";
+import React, {useEffect} from 'react';
+import {useAppDispatch} from "app/config/store";
+import {getEntities} from "app/entities/datasource/datasource.reducer";
 import PageMeta from "app/tailadmin/components/common/PageMeta";
 import PageBreadcrumb from "app/tailadmin/components/common/PageBreadCrumb";
 import ComponentCard from "app/tailadmin/components/common/ComponentCard";
 import DatasourceTable from "app/entities/datasource/datasource-table";
+import {PlusIcon} from "app/tailadmin/icons";
 
 const Datasource = () => {
-  const dispatch = useAppDispatch();
+    const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    dispatch(getEntities({}));
-  }, []);
+    useEffect(() => {
+        dispatch(getEntities({}));
+    }, []);
 
-  return (
-      <>
-        <PageMeta
-            title="React.js Basic Tables Dashboard | TailAdmin - Next.js Admin Dashboard Template"
-            description="This is React.js Basic Tables Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
-        />
-        <PageBreadcrumb pageTitle="Basic Tables"/>
-        <div className="space-y-6">
-          <ComponentCard title="Basic Table 1">
-            <DatasourceTable/>
-          </ComponentCard>
-        </div>
-      </>
-  );
+    const TitleButton = () => {
+        return <button type="button"
+                       className="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800
+                    inline-flex items-center gap-2">
+            <PlusIcon fill="white"/>
+            Extra small
+        </button>
+    }
+
+
+    return (
+        <>
+            <PageMeta
+                title="React.js Basic Tables Dashboard | TailAdmin - Next.js Admin Dashboard Template"
+                description="This is React.js Basic Tables Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+            />
+            <PageBreadcrumb pageTitle="Datasource"/>
+            <div className="space-y-6">
+                <ComponentCard title={<TitleButton/>}>
+                    <DatasourceTable/>
+                </ComponentCard>
+            </div>
+        </>
+    );
 }
 
 export default Datasource;

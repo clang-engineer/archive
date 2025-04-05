35ab3fd1a0e291655299388a5a0002db3b4579cd
clang-engineer <clang.engineer@gmail.com>
2025-04-05 19:46:41 +0900

    modify: datasource-update.tsx - add formik config

--- src/main/webapp/app/entities/datasource/datasource-update.tsx
+++ src/main/webapp/app/entities/datasource/datasource-update.tsx
@@ -11,11 +11,22 @@ import {
 } from "app/shacdn/components/ui/dialog"
 import {Input} from "app/shacdn/components/ui/input"
 import {Label} from "app/shacdn/components/ui/label"
-import {defaultValue, IDatasource} from "app/shared/model/datasource.model";
 import {Switch} from "app/shacdn/components/ui/switch";
+import {useAppDispatch, useAppSelector} from "app/config/store";
+import {createEntity} from "app/entities/datasource/datasource.reducer";
+
+import {useFormik} from 'formik';
+import * as yup from 'yup';
 
 const DatasourceUpdate = React.forwardRef((_, ref) => {
-    const [datasource, setDatasource] = useState<IDatasource>(defaultValue);
+    React.useImperativeHandle(ref, () => ({
+        open: handleOpen,
+        close: handleClose,
+    }));
+
+    const dispatch = useAppDispatch();
+
+    const datasourceEntity = useAppSelector(state => state.datasource.entity);
 
     const [open, setOpen] = useState(false)
 
@@ -27,47 +38,72 @@ const DatasourceUpdate = React.forwardRef((_, ref) => {
         setOpen(false)
     }
 
-    React.useImperativeHandle(ref, () => ({
-        open: handleOpen,
-        close: handleClose,
-    }));
+    const formik = useFormik({
+        initialValues: {
+            id: null,
+            title: '',
+            description: '',
+            activated: false,
+        },
+        validationSchema: yup.object({
+            id: yup.number().nullable('id must not be null'),
+            title: yup
+                .string()
+                .min(5, 'length must be at least 5 characters')
+                .max(100, 'length must be at most 100 characters')
+                .required('This field is required'),
+            description: yup.string(),
+            activated: yup.boolean().required('This field is required'),
+        }),
+        onSubmit(values) {
+            dispatch(createEntity(values));
+            handleClose();
+        },
+    });
+
 
     return (
         <Dialog open={open} onOpenChange={setOpen}>
             <DialogContent className="sm:max-w-[425px]" onInteractOutside={(e) => e.preventDefault()}>
-                <DialogHeader>
-                    <DialogTitle>Add Datasource</DialogTitle>
-                    <DialogDescription>
-                        Make sure to add all the necessary information for your datasource.
-                    </DialogDescription>
-                </DialogHeader>
-                <div className="grid gap-4 py-4">
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="title" className="text-right">
-                            Title
-                        </Label>
-                        <Input id="title" className="col-span-3" value={datasource.title}/>
+                <form onSubmit={formik.handleSubmit}>
+                    <DialogHeader>
+                        <DialogTitle>Add Datasource</DialogTitle>
+                        <DialogDescription>
+                            Make sure to add all the necessary information for your datasource.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4 py-4">
+                        <div className="grid grid-cols-4 items-center gap-4">
+                            <Label htmlFor="title" className="text-right">
+                                Title
+                            </Label>
+                            <Input id="title" className="col-span-3" defaultValue={datasourceEntity.title}
+                                   onChange={formik.handleChange}
+                            />
+                        </div>
+                        <div className="grid grid-cols-4 items-center gap-4">
+                            <Label htmlFor="description" className="text-right">
+                                Description
+                            </Label>
+                            <Input id="description" className="col-span-3"
+                                   defaultChecked={datasourceEntity.description}
+                                   onChange={formik.handleChange}
+                            />
+                        </div>
+                        <div className="grid grid-cols-4 items-center gap-4">
+                            <Label htmlFor="activated" className="text-right">
+                                Activated
+                            </Label>
+                            <Switch id="activated" className="col-span-3"
+                                    defaultChecked={true}
+                                    onCheckedChange={(checked) => formik.setFieldValue('activated', checked)}
+                            />
+                        </div>
                     </div>
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="description" className="text-right">
-                            Description
-                        </Label>
-                        <Input id="description" className="col-span-3"
-                               value={datasource.description}/>
-                    </div>
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="activated" className="text-right">
-                            Activated
-                        </Label>
-                        <Switch id="activated" className="col-span-3"
-                                defaultChecked={true}
-                        />
-                    </div>
-
-                </div>
-                <DialogFooter>
-                    <Button type="submit">Save changes</Button>
-                </DialogFooter>
+                    <DialogFooter>
+                        <Button type="submit">Create</Button>
+                    </DialogFooter>
+                </form>
             </DialogContent>
         </Dialog>
     )
